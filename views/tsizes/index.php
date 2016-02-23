aa3dd0618a6f805e658d84d4202c2835f8fbdbeb
cratos <cjf10785@gmail.com>
2016-02-22 20:53:44 -0600

    updated title

--- views/tsizes/index.php
+++ views/tsizes/index.php
@@ -6,7 +6,7 @@ use yii\grid\GridView;
 /* @var $this yii\web\View */
 /* @var $dataProvider yii\data\ActiveDataProvider */
 
-$this->title = 'Tsizes';
+$this->title = 'T-Shirt Sizes';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="tsizes-index">
@@ -14,7 +14,7 @@ $this->params['breadcrumbs'][] = $this->title;
     <h1><?= Html::encode($this->title) ?></h1>
 
     <p>
-        <?= Html::a('Create Tsizes', ['create'], ['class' => 'btn btn-success']) ?>
+        <?= Html::a('Create T-Shirt Size', ['create'], ['class' => 'btn btn-success']) ?>
     </p>
 
     <?= GridView::widget([
