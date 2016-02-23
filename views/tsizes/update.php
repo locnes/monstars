034a1fc470006b5202f6af377254a75a4bf34e4d
cratos <cjf10785@gmail.com>
2016-02-22 20:24:27 -0600

    updated title

--- views/tsizes/update.php
+++ views/tsizes/update.php
@@ -5,7 +5,7 @@ use yii\helpers\Html;
 /* @var $this yii\web\View */
 /* @var $model app\models\Tsizes */
 
-$this->title = 'Update Tsizes: ' . ' ' . $model->id;
+$this->title = 'Update T-shirt size: ' . ' ' . $model->type_name;
 $this->params['breadcrumbs'][] = ['label' => 'Tsizes', 'url' => ['index']];
 $this->params['breadcrumbs'][] = ['label' => $model->id, 'url' => ['view', 'id' => $model->id]];
 $this->params['breadcrumbs'][] = 'Update';
