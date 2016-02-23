7efb5383583981fbdd5ae9b72e44f2840396ad3d
keith <krolsen87@gmail.com>
2016-02-22 19:41:11 -0600

    created gii-genereated "views" table

---
+++ views/tsizes/_form.php
@@ -0,0 +1,29 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+
+/* @var $this yii\web\View */
+/* @var $model app\models\Tsizes */
+/* @var $form yii\widgets\ActiveForm */
+?>
+
+<div class="tsizes-form">
+
+    <?php $form = ActiveForm::begin(); ?>
+
+    <?= $form->field($model, 'type_name')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'price_add')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'status')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'sort_order')->textInput() ?>
+
+    <div class="form-group">
+        <?= Html::submitButton($model->isNewRecord ? 'Create' : 'Update', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+
+</div>
