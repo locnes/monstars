01c0e0e28a0fec9d8e7e1569230d82a6f394c479
cratos <cjf10785@gmail.com>
2016-02-22 19:25:58 -0600

    created new categors

---
+++ models/Tsizes.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "Tsizes".
+ *
+ * @property integer $id
+ * @property string $type_name
+ * @property string $price_add
+ * @property string $status
+ * @property integer $sort_order
+ */
+class Tsizes extends \yii\db\ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'Tsizes';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['type_name', 'price_add', 'sort_order'], 'required'],
+            [['price_add'], 'number'],
+            [['sort_order'], 'integer'],
+            [['type_name'], 'string', 'max' => 2],
+            [['status'], 'string', 'max' => 1],
+            [['type_name'], 'unique']
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'type_name' => 'Type Name',
+            'price_add' => 'Price Add',
+            'status' => 'Status',
+            'sort_order' => 'Sort Order',
+        ];
+    }
+}
