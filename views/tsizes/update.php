efd912a0c29cd41fe9c220fa50efc97f3543e155
cratos <cjf10785@gmail.com>
2016-03-07 20:13:25 -0600

    cleaning up inner face of Status, live & not live.

--- views/tsizes/update.php
+++ views/tsizes/update.php
@@ -5,7 +5,7 @@ use yii\helpers\Html;
 /* @var $this yii\web\View */
 /* @var $model app\models\Tsizes */
 
-$this->title = 'Update T-shirt size: ' . ' ' . $model->type_name;
+$this->title = 'Update Size: ' . ' ' . $model->type_name;
 $this->params['breadcrumbs'][] = ['label' => 'T-shirt size', 'url' => ['Size']];
 $this->params['breadcrumbs'][] = ['label' => $model->id, 'url' => ['view', 'id' => $model->id]];
 $this->params['breadcrumbs'][] = 'Update';
