b3a0f5a98965210973f361a1437f54851bf5affd
cratos <cjf10785@gmail.com>
2016-03-14 19:59:48 -0500

    changed title of t-shirt designs for main page.

--- views/tdesign/index.php
+++ views/tdesign/index.php
@@ -6,7 +6,7 @@ use yii\grid\GridView;
 /* @var $this yii\web\View */
 /* @var $dataProvider yii\data\ActiveDataProvider */
 
-$this->title = 'Tdesigns';
+$this->title = 'T-shirt Designs';
 $this->params['breadcrumbs'][] = "Design";
 ?>
 <div class="tdesign-index">
