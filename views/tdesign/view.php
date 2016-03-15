e43aadbf4769bcea5a4a7885b13f1a2bed3b8eac
cratos <cjf10785@gmail.com>
2016-03-14 19:53:09 -0500

    models edit to Tdesign with categoryId

--- views/tdesign/view.php
+++ views/tdesign/view.php
@@ -28,7 +28,7 @@ $this->params['breadcrumbs'][] = $this->title;
     <?= DetailView::widget([
         'model' => $model,
         'attributes' => [
-            'id',
+            //'id',
             'title',
             'price',
             'description:ntext',
