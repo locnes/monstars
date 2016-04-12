800961c0a1ff6872e9047c125477ee58c9c0a4b7
keith <krolsen87@gmail.com>
2016-04-11 21:08:06 -0500

    Created the frontend layout view to hold future views for checkout

--- views/layouts/frontEnd.php
+++ views/layouts/frontEnd.php
@@ -4,10 +4,9 @@
 /* @var $content string */
 
 use app\assets\AppAsset;
-use yii\bootstrap\Nav;
-use yii\bootstrap\NavBar;
+
 use yii\helpers\Html;
-use yii\widgets\Breadcrumbs;
+
 
 AppAsset::register($this);
 ?>
