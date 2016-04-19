8f710470752fce896d70ee22af82397cd1d4bbde
Nikolai Langlois <nlangloi10@umphysicians.umn.edu>
2016-04-18 19:33:59 -0500

    Updated the Orderform models with the appropriate validation for each stage.

---
+++ models/OrderformStep3.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: nlangloi10
+ * Date: 4/18/16
+ * Time: 5:18 PM
+ */
+
+namespace app\models;
+
+use yii\base\Model;
+
+
+class OrderformStep3 extends Model
+{
+    public $first_name;
+    public $last_name;
+    public $address;
+    public $city;
+    public $state;
+    public $zipcode;
+    public $email;
+    public $phone_number;
+
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['first_name', 'last_name', 'address', 'city', 'state', 'zipcode', 'email', 'phone_number'], 'required'],
+            ['email', 'email'],
+            [['first_name', 'last_name', 'address', 'city', 'state', 'zipcode', 'email', 'phone_number'], 'safe'],
+        ];
+    }
+
+}
---
+++ models/OrderformStep4.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: nlangloi10
+ * Date: 4/18/16
+ * Time: 5:18 PM
+ */
+
+namespace app\models;
+
+use yii\base\Model;
+
+
+class OrderformStep4 extends Model
+{
+}
