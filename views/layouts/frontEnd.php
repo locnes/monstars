22ea16d4695d409a59f5cc1566bd0c8133afad42
keith <krolsen87@gmail.com>
2016-04-25 19:00:09 -0500

    Added background graphic attached graphic to css with a custom class removed the purple background from the all other pages but frontEnd.layout

--- views/layouts/frontEnd.php
+++ views/layouts/frontEnd.php
@@ -19,7 +19,7 @@ AppAsset::register($this);
     <title><?= Html::encode($this->title) ?></title>
     <?php $this->head() ?>
 </head>
-<body>
+<body class="orderForm">
 <?php $this->beginBody() ?>
 
 <div class="col-md-12">
@@ -115,7 +115,7 @@ AppAsset::register($this);
 
 <footer class="footer">
     <div class="container">
-        <p class="pull-left">&copy; Monstars <?= date('Y') ?></p>
+        <p class="pull-left">&copy; Cratolsen <?= date('Y') ?></p>
 
         <p class="pull-right"><?//= Yii::powered() ?></p>
     </div>
