6c466759dbd4cea4ba0dec274e7b8f7e54618351
Nikolai Langlois <nlangloi10@umphysicians.umn.edu>
2016-05-02 16:24:55 -0500

    Added zipcode validation (though the field isn't currently required) on step3 view of order/form app.

--- models/Step3.php
+++ models/Step3.php
@@ -10,6 +10,12 @@ namespace app\models;
 
 use yii\base\Model;
 
+/*
+ * Though I'm not using Kartik's masked-input extension here yet, it's worth looking at his demo page to see just
+ * what can be done. It's pretty cool stuff.
+ * http://demos.krajee.com/masked-input
+ */
+
 
 class Step3 extends Model
 {
@@ -31,6 +37,13 @@ class Step3 extends Model
         return [
             //[['first_name', 'last_name', 'address', 'city', 'state', 'zipcode', 'email', 'phone_number'], 'required'],
             ['email', 'email'],
+            ['zipcode', 'match',
+                'pattern' => '/^\d{5}(?:[-\s]\d{4})?$/i',
+                'message' => 'Zip code must be comprised of the following formats: 99999, 99999-9999 or 99999 9999.',
+            ],
+            // Helpful resource on Yii2 validators:
+            // http://code.tutsplus.com/tutorials/how-to-program-with-yii2-specialized-validations--cms-23427
+
             [['first_name', 'last_name', 'address', 'city', 'state', 'zipcode', 'email', 'phone_number'], 'safe'],
         ];
     }
