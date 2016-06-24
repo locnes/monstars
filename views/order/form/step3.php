417bb51406cf2a71814ba71653cd8aa154f12388
Nikolai Langlois <nlangloi10@umphysicians.umn.edu>
2016-06-24 00:24:37 -0500

    Cleaned up code formatting that PHP Storm had wiped out.

--- views/order/form/step3.php
+++ views/order/form/step3.php
@@ -21,11 +21,6 @@ echo WizardMenu::widget([
         ['label' => '<i class="glyphicon glyphicon-picture"></i>'],
     ],
     */
-
-
-
-
-
     'encodeLabels' => false,
 ]);
 echo WizardMenu::post();
@@ -35,23 +30,35 @@ echo Html::tag('h3', Html::encode("Checkout"));
 
 $form = ActiveForm::begin();
 // All of the code to lay out the "step 3" stage should live right here, like what I have below:
-echo $form->field($model, 'first_name');
-echo $form->field($model, 'last_name');
-echo $form->field($model, 'address');
-echo $form->field($model, 'city');
-echo $form->field($model, 'state');
-echo $form->field($model, 'zipcode');
-echo $form->field($model, 'email')->textInput()->input('email', ['placeholder' => "Enter your email address"]);
-echo $form->field($model, 'phone_number')->widget(MaskedInput::className(),
-    [
-        'mask' => '999-999-9999',
-    ]
-);
 
-echo Html::beginTag('div', ['class' => 'form-row buttons']);
-echo Html::submitButton('Prev', ['class' => 'button', 'name' => 'prev', 'value' => 'prev']);
-echo Html::submitButton('Next', ['class' => 'button', 'name' => 'next', 'value' => 'next']);
-echo Html::submitButton('Pause', ['class' => 'button', 'name' => 'pause', 'value' => 'pause']);
-echo Html::submitButton('Cancel', ['class' => 'button', 'name' => 'cancel', 'value' => 'pause']);
-echo Html::endTag('div');
+    echo Html::beginTag('div', ['class' => 'row']);
+        // Left column
+        echo Html::beginTag('div', ['class' => 'col-md-6']);
+        echo $form->field($model, 'first_name');
+        echo $form->field($model, 'address');
+        echo $form->field($model, 'state');
+        echo $form->field($model, 'email')->textInput()->input('email', ['placeholder' => "Enter your email address"]);
+        echo Html::endTag('div');
+
+        // Right column
+        echo Html::beginTag('div', ['class' => 'col-md-6']);
+        echo $form->field($model, 'last_name');
+        echo $form->field($model, 'city');
+        echo $form->field($model, 'zipcode');
+        echo $form->field($model, 'phone_number')->widget(MaskedInput::className(),
+            [
+                'mask' => '999-999-9999',
+            ]
+        );
+        echo Html::endTag('div');
+    echo Html::endTag('div');
+
+
+    echo Html::beginTag('div', ['class' => 'form-row buttons']);
+        echo Html::submitButton('Prev', ['class' => 'button', 'name' => 'prev', 'value' => 'prev']);
+        echo Html::submitButton('Next', ['class' => 'button', 'name' => 'next', 'value' => 'next']);
+        echo Html::submitButton('Pause', ['class' => 'button', 'name' => 'pause', 'value' => 'pause']);
+        echo Html::submitButton('Cancel', ['class' => 'button', 'name' => 'cancel', 'value' => 'pause']);
+    echo Html::endTag('div');
+
 ActiveForm::end();
